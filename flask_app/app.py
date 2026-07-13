843e03f845f7d1abced1bd42ab3f26f95038cc31
Abhay Soni <techwolfabhay@gmail.com>
2026-07-13 12:17:49 +0530

    flask dashboard

---
+++ flask_app/app.py
@@ -0,0 +1,15 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    data = {'revenue': 125000, 'inventory': 340, 'recommended_price': 2499}
+    return render_template('dashboard.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
